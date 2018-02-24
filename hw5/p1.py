1fb6735ead897f6df5b58205c5eb4c8a06114d06
iqbalO2018 <oiqbal@sjnma.org>
2018-02-24 12:52:01 -0600

    p1.py unfinished

---
+++ hw5/p1.py
@@ -0,0 +1,11 @@
+temp = float(raw_input("Enter the tempreture on your thermometer"))
+#have convo with tempinputer
+if(temp <= 96.0):
+	print("Are you cold?")
+response = raw_input()
+if(response = "yes")
+	print("Try dressing up for the winter to keep your tempreture up")
+response = raw_input()
+if(response = "no"
+	print("hmph, you must be cold blooded")
+
