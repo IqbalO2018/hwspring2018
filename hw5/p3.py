1193da4cb74ac2831c33c65f6cdfd359f207fbfe
iqbalO2018 <oiqbal@sjnma.org>
2018-02-28 22:36:40 -0600

    FINALLY OVER!

--- hw5/p3.py
+++ hw5/p3.py
@@ -4,5 +4,22 @@ s=raw_input("please enter any amount of words, seperated by commas")
 lower = s.lower()
 list = s.split(",")
 numofwords = len(list)
-print(numofwords)
-if(numofwords
+word = list
+word1 = word[0]
+word2 = word[1]
+word3 = word[2]
+word4 = word[3]
+word5 = word[4]
+
+
+if(numofwords % 2 != 0):
+	print("that is odd")
+	if("llama" in lower):
+			print(list)
+
+if(len(word1) % 2 != 0):
+	if(len(word2) % 2 != 0):
+		if(len(word3) % 2 != 0):
+			if(len(word4) % 2 != 0):
+				if(len(word4) % 2 != 0):
+					print("this is odd")
