71200038951507574061c0a68cc5c084349734ed
iqbalO2018 <oiqbal@sjnma.org>
2018-02-28 21:12:17 -0600

    homework 5

--- hw5/p1.py
+++ hw5/p1.py
@@ -1,11 +1,25 @@
-temp = float(raw_input("Enter the tempreture on your thermometer"))
-#have convo with tempinputer
-if(temp <= 96.0):
-	print("Are you cold?")
-response = raw_input()
-if(response = "yes")
-	print("Try dressing up for the winter to keep your tempreture up")
-response = raw_input()
-if(response = "no"
-	print("hmph, you must be cold blooded")
+temp = float(raw_input("enter the tempreture on your thermometer"))
 
+if(temp > 96.0):
+	response = raw_input ("Are you warm?")
+	if(response == "yes"):
+		response = raw_input ("Try dressing up for the winter to keepyour tempreture up")
+		if(response == "no"):
+			print("Hmph, you must be cold blooded")  
+else:
+			print("close to normal, you may want to try again later")
+
+if(temp < 99.0):
+	response = raw_input ("do you feel cold?")
+	if(response == "yes"):
+		response = raw_input("you may be running a fever")
+	if(response == "no"):
+		print("you must be warm blooded")
+else:
+		print("close to normal, you may want to try again later")
+
+
+if(temp == 98.6):
+	print("congratulations, you are normal and healthy")
+else:
+	print("close to normal, you may want to try again later")
---
+++ hw5/p3.py
@@ -0,0 +1,8 @@
+#words = raw_input(",")("enter any amount of words, seperated by coommas")
+
+s=raw_input("please enter any amount of words, seperated by commas")
+lower = s.lower()
+list = s.split(",")
+numofwords = len(list)
+print(numofwords)
+if(numofwords
